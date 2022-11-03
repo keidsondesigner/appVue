5b7a308e43af3b5d7693a7c5f87288ca49a522fc
keidsondesigner <keidsondesigner@gmail.com>
2022-11-03 10:31:38 -0400

    imports

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DefaultButton text="Olá mundo" />
+  <DefaultInputVue
+    text="Data de Nascimento"
+    message="Valor incorreto, por favor digite novamente!"
+  />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import DefaultButton from './components/DefaultButton.vue'
+import DefaultInputVue from './components/DefaultInput.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    DefaultButton,
+    DefaultInputVue,
   }
 }
 </script>
 
 <style>
+* {
+  padding:0;
+  margin:0;
+  vertical-align:baseline;
+  list-style:none;
+  border:0
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
